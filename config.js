5aaf7770aea9ae3ef6be98ecf891a06e7fd741ba
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-21 02:56:19 -0700

    config edit

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://amuser:ampassword1@ds255262.mlab.com:55262/asset_manager',
+        process.env.DATABASE_URL || 'mongodb://localhost:27017/asset-manager'
   TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL || 'mongodb://testuser:testing1@ds155292.mlab.com:55292/asset_manager_test',
+        process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test'
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '15m'
 
