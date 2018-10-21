cb57b21f0fc41eb08293b86dcdd78c43bb56e021
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-21 03:15:26 -0700

    config edit

--- config.js
+++ config.js
@@ -5,10 +5,9 @@ require('dotenv').load();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost:27017/asset-manager'
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/asset-manager',
   TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test'
+        process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '15m'
 
