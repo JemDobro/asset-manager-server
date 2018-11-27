e9063ec01e46e7a3eb7fe934fbacf91f5ae18a21
Jemadean Dobrosielski <jemadean@gmail.com>
2018-11-26 19:09:03 -0800

    changed mongodb_uri to database_url -- heroku not connecting to the db

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/asset-manager',
+  DATABASE_URL: process.env.DATABASE_URL,
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test',
   JWT_SECRET: process.env.JWT_SECRET,
